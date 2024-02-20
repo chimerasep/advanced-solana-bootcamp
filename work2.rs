2288b84919c9f0c3cc53216a1b8721361546ea75
Emre Can Şahin <108821600+chimerasep@users.noreply.github.com>
2024-02-20 18:55:56 +0300

    Create work2.rs

---
+++ work2.rs
@@ -0,0 +1,24 @@
+enum Publication {
+    Book(Book),
+    Magazine(Magazine),
+}
+
+struct Book {
+    title: String,
+    author: String,
+    page_count: u32,
+}
+
+struct Magazine {
+    title: String,
+    issue: u32,
+    topic: String,
+}
+
+fn print_publications(publications: Vec<Publication>) {
+    
+      
+}
+
+fn main() {
+  }
